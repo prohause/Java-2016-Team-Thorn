a68fb04fecea582d87f31b417d6e73b9fb279d36
KNikovv <kaloyannikov@gmail.com>
2016-04-01 14:04:24 +0300

    Removed Swing display Classes , and added Slick

--- src/PackMan.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by proha on 28-Mar-16.
- */
-public class PackMan {
-    public static void main(String[] args) {
-        System.out.println("This is just a test");
-    }
-
-}
--- src/game/Launcher.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-import display.Display;
-
-public class Launcher {
-    public static void main(String[] args) {
-        new Game("PackNakov");
-    }
-}
