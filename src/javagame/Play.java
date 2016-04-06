9c6ee704786881409a58ed57c105c54ff826c1f1
prohause <prohause@abv.bg>
2016-04-06 15:36:52 +0300

    Added movement
    Signed-off-by: prohause <prohause@abv.bg>

--- src/javagame/Play.java
+++ src/javagame/Play.java
@@ -1,23 +1,69 @@
 package javagame;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.state.*;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Play extends BasicGameState {
-    public Play(int state){
+
+    Image face;
+    float facex = 0;
+    float facey = 0;
+    Music openingmusic;
+
+    public Play(int state) {
 
     }
-    public void init(GameContainer gc,StateBasedGame sbg) throws SlickException{
+
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        face = new Image("res/face.png");
+        gc.setShowFPS(false);
+        openingmusic = new Music("res/pacnakov_openingmusic.ogg");
+        openingmusic.loop();
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+
+        g.drawImage(face, facex, facey);
 
     }
 
-    public void render(GameContainer gc,StateBasedGame sbg,Graphics g) throws SlickException{
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+
+        Input input = gc.getInput();
+
+
+
+            if (input.isKeyDown(Input.KEY_UP)) {
+                facey -= 0.5;
+            }
+            if (input.isKeyDown(Input.KEY_DOWN)) {
+                facey += 0.5;
+            }
+            if (input.isKeyDown(Input.KEY_LEFT)) {
+                facex -= 0.5;
+            }
+            if (input.isKeyDown(Input.KEY_RIGHT)) {
+                facex += 0.5;
+            }
+        if (facex < 0){
+            facex = 0;
+        }
+        if (facex > 725){
+            facex = 725;
+        }
+        if (facey < 0){
+            facey = 0;
+        }
+        if (facey > 523){
+            facey = 523;
+        }
+
+
 
     }
-    public void update(GameContainer gc,StateBasedGame sbg,int delta) throws SlickException{
 
-    }
-    public int getID(){
+    public int getID() {
         return 1;
     }
 }
