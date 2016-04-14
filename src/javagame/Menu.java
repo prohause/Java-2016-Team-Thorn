34e54312c9a6464bfb0734b6347bf8d0141701db
Veronika Dzhambazova <veronika.dzhambazova@abv.bg>
2016-04-14 17:18:28 +0300

    refactor

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -28,49 +28,48 @@ public class Menu extends BasicGameState {
     }
 
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-        int xpos = Mouse.getX();
-        int ypos = Mouse.getY();
+        int xPosition = Mouse.getX();
+        int yPosition = Mouse.getY();
         launcherMenu.draw(0, 0);
         graphics.drawString(mouse, 100, 50);
 
         // if mouse hovering over the area a button is drawed
-        if ((xpos > 445 && xpos < 668) && (ypos > 303 && ypos < 355)) {
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 303 && yPosition < 355)) {
             playButton.draw(440, 242);
         }
 
-        if ((xpos > 445 && xpos < 668) && (ypos > 220 && ypos < 275)) {
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 220 && yPosition < 275)) {
             highscoreButton.draw(440, 322);
         }
 
-        if ((xpos > 445 && xpos < 668) && (ypos > 140 && ypos < 195)) {
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 140 && yPosition < 195)) {
             optionsButton.draw(440, 405);
         }
 
-        if ((xpos > 445 && xpos < 668) && (ypos > 60 && ypos < 115)) {
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 60 && yPosition < 115)) {
             exitButton.draw(440, 485);
         }
     }
 
-
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
-        int xpos = Mouse.getX();
-        int ypos = Mouse.getY();
-        mouse = "xpos" + xpos + "ypos" + ypos;
-        if ((xpos > 445 && xpos < 668) && (ypos > 303 && ypos < 355)) {
+        int xPosition = Mouse.getX();
+        int yPosition = Mouse.getY();
+        mouse = "xPosition" + xPosition + "yPosition" + yPosition;
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 303 && yPosition < 355)) {
             if (Mouse.isButtonDown(0)) {
                 openingmusic.stop();
                 stateBasedGame.enterState(1); //if button is clicked inside the area : Start a new game
             }
         }
 
-        if ((xpos > 445 && xpos < 668) && (ypos > 220 && ypos < 275)) {
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 220 && yPosition < 275)) {
             if (Mouse.isButtonDown(0)) {
                 openingmusic.stop();
                 stateBasedGame.enterState(2); //if button is clicked inside the area : Start a new game
             }
         }
 
-        if ((xpos > 445 && xpos < 668) && (ypos > 60 && ypos < 115)) {
+        if ((xPosition > 445 && xPosition < 668) && (yPosition > 60 && yPosition < 115)) {
             if (Mouse.isButtonDown(0)) {
                 openingmusic.stop();
                 System.exit(0); // if button is clicked exit the Launcher
